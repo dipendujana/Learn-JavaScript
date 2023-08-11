c60ef6c0bb720a9a3ea0072297b893c9307fc9ba
dipendujana5 <120547835+dipendujana5@users.noreply.github.com>
2023-08-11 06:09:25 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,24 @@
+"use strict";//treat all JS code as newer version
+// alert(3+3)//we are using nodejs,not browser
+
+console.log(3+
+    3); //code readability should be high
+console.log("Dipendu")
+
+let name = "Dipendu"
+let age = 18
+let isLonggedIn = false
+let state;
+
+// name => 2 to power 53
+// bigint
+// string => ""
+// boolean =>true/false
+// null => standalone value
+// undefinen =>
+// symbol => unique
+
+// object 
+console.log(typeof undefinen) // undefinen
+console.log(typeof null) //object
+
