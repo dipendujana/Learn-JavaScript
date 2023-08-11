9fe321580372a64825826506535d2fb7c8a76b0c
dipendujana5 <120547835+dipendujana5@users.noreply.github.com>
2023-08-11 13:54:45 +0000

    conversion_operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,41 @@
+let score = "dipendu"
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+// "33" => 33
+// "33abc" =>NaN
+// treu => 1; false => 0
+
+let isLonggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLonggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => treu; 0 => false
+// "" => false
+// "dipendu" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(someNumber)
+console.log(typeof stringNumber)
+
+
+/**********************************  Operations  ************************************/
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+console.log(2 + 2)
+console.log(2 - 2)
+console.log(2 * 2)
+console.log(2 ** 2)
+console.log(2 / 2)
+console.log(2 % 2)
