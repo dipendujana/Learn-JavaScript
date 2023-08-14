cff5452004384a9b3d3e1c632d258146d463f48f
dipendujana5 <120547835+dipendujana5@users.noreply.github.com>
2023-08-14 11:12:06 +0000

    number and maths

---
+++ 01_basics/06_number_and_math.js
@@ -0,0 +1,34 @@
+// const score = 400
+//  console.log(score);
+
+// const balance = new Number(100)
+//  console.log(balance);
+
+//  console.log(balance.toString().length);
+//  console.log(balance.toFixed(2));
+
+//  const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(4));
+
+// const hundreds = 1000000
+//  console.log(hundreds.toLocaleString('en-IN'));
+
+// // +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+//  console.log(Math);
+//  console.log(Math.abs(-4));  //abs mathard a -4 console 4 hoye jay mane holo -ve +ve hoye jay kin +ve +ve thake 
+//  console.log(Math.round(4.6)); // ans is => 5 karon 4.6 ache tai 5 
+//  console.log(Math.ceil(4.2));  // ans is 5 because celi mathherd use kora hoyeche 
+//  console.log(Math.floor(4.9)); //ans is 4 because floor mathherd use kora hoyeche 
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
