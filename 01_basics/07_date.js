2f25d2dd6b42620e610c43af1062d06459cf324d
dipendujana5 <120547835+dipendujana5@users.noreply.github.com>
2023-08-14 11:53:23 +0000

    date in js

---
+++ 01_basics/07_date.js
@@ -0,0 +1,26 @@
+// // Date
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate); // date ek object hei javaScript main
+
+// let myCreatedDate = new Date(2023,11, 23)
+// console.log(myCreatedDate.toDateString());
+
+let myTimeStanp = Date.now()
+console.log(myTimeStanp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+// `${newDate.getDay()} and the time `
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+    
+})
