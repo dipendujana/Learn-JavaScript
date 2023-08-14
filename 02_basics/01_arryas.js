4c85b203b939f5a289061444d5f16b4c03f1e3ad
dipendujana5 <120547835+dipendujana5@users.noreply.github.com>
2023-08-14 14:27:38 +0000

    array

---
+++ 02_basics/01_arryas.js
@@ -0,0 +1,19 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeors = ["shaktiman", "naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr[1]);
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)  // unshift array ar samne ekta element add kore dey in this case [9,0,1,2,3,4,5]
+
+// myArr.shift()  // shift ar khetre samner ekta element chole jay like [1,2,3,4,5]
+ console.log(myArr);
+
