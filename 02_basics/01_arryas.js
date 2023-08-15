a041691f7fee7f8e836c61639da6130bac9fa0d8
dipendujana5 <120547835+dipendujana5@users.noreply.github.com>
2023-08-15 10:46:49 +0000

    arrya 1

--- 02_basics/01_arryas.js
+++ 02_basics/01_arryas.js
@@ -3,7 +3,7 @@
 const myArr = [0, 1, 2, 3, 4, 5]
 const myHeors = ["shaktiman", "naagraj"]
 
-const myArr2 = new Array(1, 2, 3, 4)
+ const myArr2 = new Array(1, 2, 3, 4) //ay rokom vabeo arrya dicler kora jay
 // console.log(myArr[1]);
 
 // Array methods
@@ -12,8 +12,30 @@ const myArr2 = new Array(1, 2, 3, 4)
 // myArr.push(7)
 // myArr.pop()
 
-// myArr.unshift(9)  // unshift array ar samne ekta element add kore dey in this case [9,0,1,2,3,4,5]
+// myArr.unshift(9) // unshift array ar samne ekta element add kore dey in this case [9,0,1,2,3,4,5]
 
 // myArr.shift()  // shift ar khetre samner ekta element chole jay like [1,2,3,4,5]
- console.log(myArr);
+//console.log(myArr);
+
+ // console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log( newArr);
+
+
+// slice, splice
+
+console.log("A ", myArr); // A  [ 0, 1, 2, 3, 4, 5 ]
+const myn1 = myArr.slice(1, 3)  
+
+console.log(myn1);         // [ 1, 2 ]
+console.log("B ", myArr); // B  [ 0, 1, 2, 3, 4, 5 ]
+
+
+const myn2 = myArr.splice(1, 3)
+console.log("C ", myArr);  // C  [ 0, 4, 5 ]  // slice print hobar por main arr tay change hoye jai
+console.log(myn2); // [ 1, 2, 3 ]
 
