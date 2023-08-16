6bd17d9d0211b23b45b6d345f7c3dedcfdd87518
dipendujana5 <120547835+dipendujana5@users.noreply.github.com>
2023-08-16 04:10:29 +0000

    arrya1.5

---
+++ 02_basics/02_arryas.js
@@ -0,0 +1,19 @@
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros)
+
+ //console.log(marvel_heros);
+ //console.log(marvel_heros[3][1]);
+
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros);
+
+const all_new_heros = [...marvel_heros, ...dc_heros]  // by the help of sperad operation
+// console.log(all_new_heros);
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+const real_another_array = another_array.flat(Infinity) //" flat " ar shaje one gulo arrya ke ek songe kota jay 
+console.log(real_another_array);
+
