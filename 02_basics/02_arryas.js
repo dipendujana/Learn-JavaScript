c16720ffd5b54f0a0743fb04393b204f863ad1b9
dipendujana5 <120547835+dipendujana5@users.noreply.github.com>
2023-08-17 03:20:08 +0000

    arrya 2

--- 02_basics/02_arryas.js
+++ 02_basics/02_arryas.js
@@ -14,6 +14,16 @@ const all_new_heros = [...marvel_heros, ...dc_heros]  // by the help of sperad o
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
-const real_another_array = another_array.flat(Infinity) //" flat " ar shaje one gulo arrya ke ek songe kota jay 
+const real_another_array = another_array.flat(Infinity) //" flat " ar shaje one gulo arrya ke ek songe kora jay 
 console.log(real_another_array);
+
+console.log(Array.isArray("Hitesh"))
+console.log(Array.from("Hitesh"))
+console.log(Array.from({name: "hitesh"})) // interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
 
