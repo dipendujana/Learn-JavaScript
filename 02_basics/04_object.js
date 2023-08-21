96b4052cae5eca94c5e3831ca61ccdeebf46b695
dipendujana5 <120547835+dipendujana5@users.noreply.github.com>
2023-08-21 11:37:20 +0000

    object2

---
+++ 02_basics/04_object.js
@@ -0,0 +1,54 @@
+//const tenderUser =  new Object()  singleton object
+const tenderUser = {}  // non singleton object
+tenderUser.id = "123abc",
+tenderUser.name = "Dipendu",
+tenderUser.isLogeedIn = false
+//console.log(tenderUser);
+
+const regulerUser = {
+    email: "dipendu@gamil.com",
+    fullname: {
+        uderFullname: {
+            fistName: "Dipendu",
+            lastName: "Jana"
+        }
+    }
+}
+
+//console.log(regulerUser.fullname.uderFullname.fistName);
+
+//+++++++++ how to marge object +++++++++++++++++++//
+
+const obj1 = {1:"a", 2: "b"}
+const obj2 = {3:"c", 4: "d"}
+const obj3 = {5:"e", 6: "f"}
+
+//const obj = {obj1,obj2,obj3}
+//const obj = Object.assign({},obj1,obj2,obj3)
+const obj = {...obj1,...obj2,...obj3}  // usinge spader operator 
+
+//console.log(obj);
+
+//++++++++++++ Data base diye ki rokon vale ase dekha jak +++++++++++++++++//
+
+const usre = [
+    {
+        id:1,
+        email: "Dipend@gamail.com",
+    },
+    {
+        id:1,
+        email: "Dipend@gamail.com",
+    },
+    {
+        id:1,
+        email: "Dipend@gamail.com",
+    }
+]
+usre[1].email
+console.log(tenderUser);
+console.log(Object.keys(tenderUser));
+console.log(Object.values(tenderUser));
+console.log(Object.entries(tenderUser));
+
+console.log(tenderUser.hasOwnProperty('isLogeed'));
