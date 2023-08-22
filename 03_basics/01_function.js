afbc1219c10c8bdcb4dce01c2083e051aa669191
dipendujana5 <120547835+dipendujana5@users.noreply.github.com>
2023-08-22 14:22:10 +0000

    funcation

---
+++ 03_basics/01_function.js
@@ -0,0 +1,17 @@
+function sayMyName() {
+    console.log("D");
+    console.log("I");
+    console.log("P");
+    console.log("E");
+    console.log("N");
+    console.log("D");
+    console.log("U");
+}
+//sayMyName()
+
+ function addTwoNumbers(number1, number2){  // pera miter 
+
+   console.log(number1 + number2);
+ }
+
+ addTwoNumbers(3,5)  // argument
