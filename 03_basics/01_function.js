882d709afc6933797bb8e1ccb46564deae2e081b
dipendujana5 <120547835+dipendujana5@users.noreply.github.com>
2023-08-23 04:23:35 +0000

    function 1

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -9,9 +9,35 @@ function sayMyName() {
 }
 //sayMyName()
 
- function addTwoNumbers(number1, number2){  // pera miter 
+//  function addTwoNumbers(number1, number2){  // pera miter 
 
-   console.log(number1 + number2);
- }
+//    console.log(number1 + number2);
+//  }
 
- addTwoNumbers(3,5)  // argument
+function addTwoNumbers(number1, number2){  // pera miter 
+
+// let result = number1 + number2
+// return  result                    // note* return ar pore kono consol kaj kore na 
+   return number1 + number2
+
+}
+
+const result = addTwoNumbers(3,5)  // argument
+
+//console.log("Result:",result);
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+
+function logUserMessage(username ){  
+//function logUserMessage(username = "sam"){  // = "sam" ar mane defoult value
+
+ // if (username === undefined)      //ay line tao thik
+  if (!username) {                  // ay line tao lekha jay ! ulto kore dei value ke
+      console.log("Place Enter Username :)");
+      return
+  }
+  return `${username} Just logged in`
+}
+// console.log(logUserMessage("Dipendu"))
+console.log(logUserMessage()) // kono value na dile undefined Just logged in
+
