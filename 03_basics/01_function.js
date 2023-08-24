dfb85ebad0a803c4a319a9c4820a1ec0c5e21d63
dipendujana5 <120547835+dipendujana5@users.noreply.github.com>
2023-08-24 07:10:52 +0000

    function with object and arrya

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -33,11 +33,38 @@ function logUserMessage(username ){
 
  // if (username === undefined)      //ay line tao thik
   if (!username) {                  // ay line tao lekha jay ! ulto kore dei value ke
-      console.log("Place Enter Username :)");
+    //  console.log("Place Enter Username :)");
       return
   }
   return `${username} Just logged in`
 }
 // console.log(logUserMessage("Dipendu"))
-console.log(logUserMessage()) // kono value na dile undefined Just logged in
+//console.log(logUserMessage()) // kono value na dile undefined Just logged in
 
+//++++++++++++++++ function with object and arrya ++++++++++++++++++++//
+
+function calculeteCarPrice(valu1,valu2,...num1){  // ... ar mane hil rest operator abar ... spred operator ar jonno hoy
+    return num1
+}
+//console.log(calculeteCarPrice(200,400,600,800));
+
+const user = {
+    username: "Dipendu",
+    price: 199
+}
+function handelObject(anyObject){
+    console.log(`username is ${anyObject.username} price is ${anyObject.price}`);
+}
+
+// handelObject(user)
+handelObject({
+    username: "Kolkata ka banda",
+    price: 399
+})
+
+const myNewArrya = [200,300,400]
+function returnSecendValue(getArrya){
+    return getArrya[1]
+}
+console.log(returnSecendValue(myNewArrya));
+console.log(returnSecendValue([200,300,400]));
