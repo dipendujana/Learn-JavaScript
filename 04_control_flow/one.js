29423ce91887d9be6561377cad9dae9be09c2f08
dipendujana5 <120547835+dipendujana5@users.noreply.github.com>
2023-08-30 15:31:32 +0000

    switch

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -28,4 +28,31 @@ const temperature = 41
 
 // if (balance > 500) console.log("test"),console.log("test2");  //Short Hand notetion
 
+//+++++++++++++++ 
 
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+    
+// } else if (balance < 900) {
+//     console.log("less than 750");
+    
+// } else {
+//     console.log("less than 1200");
+
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
---
+++ 04_control_flow/switch.js
@@ -0,0 +1,35 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("Apil");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("Jun");
+        break;
+
+    default:
+        console.log("Defult code metch");
+        break;
+}
