ed7646ba9200087a89b75f458c5b536ce8b5d548
dipendujana5 <120547835+dipendujana5@users.noreply.github.com>
2023-08-30 07:02:32 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,31 @@
+// if
+const isUserloggedIn = true
+const temperature = 41
+
+// if ( temperature === 40 ){
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+
+// console.log("Execute");
+// <, >, <=, >=, ==, !=, ===(dupase soman hote hobe 3 = 3 hole thik, 3 = "3" eta vul, dupase num, ba string  ekpase num ekpase string hole hobe na ) , !== 
+
+//+++++++ Scope +++++++++//
+
+// const score = 200
+
+// if (score > 100) {
+//     let power = "fly" //  var power = "fly" ekhane var dile run korbe kintu , run hoba thiknoy *karon
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);    // *karon eta scope ar baire
+
+//++++ Short Hand notetion +++++//
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log("test2");  //Short Hand notetion
+
+
