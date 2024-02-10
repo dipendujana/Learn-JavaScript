53924376c1ffcd52d7bec12cc7e9c751bca77f60
dipendujana5 <120547835+dipendujana5@users.noreply.github.com>
2024-02-10 04:28:50 +0000

    change

--- 01_basics/07_date.js
+++ 01_basics/07_date.js
@@ -1,26 +1,43 @@
 // // Date
 // let myDate = new Date()
-// console.log(myDate.toString());
 // console.log(myDate.toDateString());
-// console.log(myDate.toLocaleString());
-// console.log(typeof myDate); // date ek object hei javaScript main
 
-// let myCreatedDate = new Date(2023,11, 23)
-// console.log(myCreatedDate.toDateString());
+//  console.log(myDate.toDateString());
+//  console.log(myDate.toLocaleString());
+//  console.log(typeof myDate); // date ek object hei javaScript main
 
-let myTimeStanp = Date.now()
-console.log(myTimeStanp);
-// console.log(myCreatedDate.getTime());
+//  let myCreatedDate = new Date(2023,11, 23)
+ let myCreatedDate = new Date("02-10-2024")
+
+//  console.log(myCreatedDate.toDateString());
+
+//  let myTimeStanp = Date.now()
+//  console.log(myTimeStanp);
+//  console.log(myCreatedDate.getTime());
 // console.log(Math.floor(Date.now()/1000));
 
-let newDate = new Date()
-console.log(newDate);
-console.log(newDate.getMonth() + 1);
-console.log(newDate.getDay());
+ let newDate = new Date()
+ console.log(newDate);
+ console.log(newDate.getMonth() + 1);
+// console.log(newDate.getDay());
 
 // `${newDate.getDay()} and the time `
 
-newDate.toLocaleString('default', {
-    weekday: "long",
+// newDate.toLocaleString('default', {
+    // weekday: "long",
     
-})
+// })
+
+
+
+
+
+
+
+
+
+
+// dates
+
+// let myDate = new Date()
+// console.log(myDate.toString());
