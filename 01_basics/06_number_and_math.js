327efb5f23e17b24ebf386c6dba38eaaeb4aee2a
dipendujana <120547835+dipendujana@users.noreply.github.com>
2024-04-01 18:48:07 +0000

    update

--- 01_basics/06_number_and_math.js
+++ 01_basics/06_number_and_math.js
@@ -4,23 +4,23 @@
  const balance = new Number(100)
   // console.log(balance);
 
-//
+
   // console.log(balance.toString().length);
-// 
-//  console.log(balance.toFixed(2));
 
-//  const otherNumber = 123.8966
+  // console.log(balance.toFixed(2));
 
-// console.log(otherNumber.toPrecision(4));
+ const otherNumber = 123.8966
 
-// const hundreds = 1000000
-//  console.log(hundreds.toLocaleString('en-IN'));
+//  console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
 
 // // +++++++++++++ Maths +++++++++++++++++++++++++++++
 
 //  console.log(Math);
 //  console.log(Math.abs(-4));  //abs mathard a -4 console 4 hoye jay mane holo -ve +ve hoye jay kintu +ve +ve thake 
-//  console.log(Math.round(4.6)); // ans is => 5 karon 4.6 ache tai 5 
+//  console.log(Math.round(4.5)); // ans is => 5 karon 4.6 ache tai 5 
 //  console.log(Math.ceil(4.2));  // ans is 5 because celi mathherd use kora hoyeche 
 //  console.log(Math.floor(4.9)); //ans is 4 because floor mathherd use kora hoyeche 
 //  console.log(Math.min(4, 3, 6, 8));
@@ -33,4 +33,11 @@
 const min = 1
 const max = 6
 
- console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+//  console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+
+
+
+console.log(Math.ceil(10.1));
