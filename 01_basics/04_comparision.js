370e3f80f2d66ee4bada4dd93299f222417e6cee
dipendujana <120547835+dipendujana@users.noreply.github.com>
2024-07-13 06:37:51 +0000

    comparision

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -5,16 +5,16 @@
 // console.log(2 != 1);
 
 
-// console.log("2" > 1);
-//  console.log("02" > 1);
+// console.log("2" > 1);    //true
+//  console.log("02" > 1);  //true
 
-// console.log(null > 0);
-// console.log(null == 0);
-// console.log(null >= 0);
+// console.log(null > 0);   //false
+// console.log(null == 0);  //false
+// console.log(null >= 0);  //true
 
-// console.log(undefined == 0);
-// console.log(undefined > 0);
-// console.log(undefined < 0);
+// console.log(undefined == 0); //false
+// console.log(undefined > 0);  //false
+// console.log(undefined < 0);  //false
 
 // === 
 
