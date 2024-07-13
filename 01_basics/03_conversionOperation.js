09a89e70872fdb40f5791bd183c965cfd363d41a
dipendujana <120547835+dipendujana@users.noreply.github.com>
2024-07-13 06:30:48 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -46,10 +46,10 @@ let str2 = " hitesh"
 let str3 = str1 + str2
 // console.log(str3);
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+// console.log("1" + 2);             //12
+// console.log(1 + "2");             //12
+// console.log("1" + 2 + 2);         //122
+// console.log(1 + 2 + "2");         //32
 
 // console.log( (3 + 4) * 5 % 3);
 
