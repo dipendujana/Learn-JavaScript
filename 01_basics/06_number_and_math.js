eaa823d23f51df900e9e39378e19946cd33fbb54
dipendujana <120547835+dipendujana@users.noreply.github.com>
2024-07-16 06:17:24 +0000

    number

--- 01_basics/06_number_and_math.js
+++ 01_basics/06_number_and_math.js
@@ -7,7 +7,7 @@ const balance = new Number(100)
 
 // console.log(balance.toString().length);
 
-// console.log(balance.toFixed(2));
+// console.log(balance.toFixed(2));  //E-Commerse site a use hoy
 
 const otherNumber = 123.8966
 
@@ -18,7 +18,7 @@ const hundreds = 1000000
 
 // // +++++++++++++ Maths +++++++++++++++++++++++++++++
 
- console.log(Math);
+//  console.log(Math);
 //  console.log(Math.abs(-4));  //abs mathard a -4 console 4 hoye jay mane holo -ve +ve hoye jay kintu +ve +ve thake 
 //  console.log(Math.round(4.5)); // ans is => 5 karon 4.6 ache tai 5 
 //  console.log(Math.ceil(4.2));  // ans is 5 because celi mathherd use kora hoyeche 
@@ -26,9 +26,9 @@ const hundreds = 1000000
 //  console.log(Math.min(4, 3, 6, 8));
 //  console.log(Math.max(4, 3, 6, 8));
 
- console.log(Math.random());
- console.log((Math.random()*10) + 1);
- console.log(Math.floor(Math.random()*10) + 1);
+//  console.log(Math.random());
+//  console.log((Math.random()*10) + 1);
+//  console.log(Math.floor(Math.random()*10) + 1);
 
 const min = 1
 const max = 6
@@ -41,3 +41,8 @@ const max = 6
 
 
 // console.log(Math.ceil(10.1));
+
+
+console.log(Math.random());
+console.log(Math.floor(4.6));
+console.log(Math.ceil(4.6));
