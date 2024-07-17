df9becd30fa3f1bb081b8c994244f5e26eea668e
dipendujana <120547835+dipendujana@users.noreply.github.com>
2024-07-17 06:34:58 +0000

    dates

--- 01_basics/07_date.js
+++ 01_basics/07_date.js
@@ -1,18 +1,19 @@
 // // Date
 let myDate = new Date()
-console.log(myDate.toDateString());
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate); // date ek object hei javaScript main
 
-// //  console.log(myDate.toDateString());
-// //  console.log(myDate.toLocaleString());
-//  console.log(typeof myDate); // date ek object hei javaScript main
+let myCreatedDates = new Date(2023,11, 23)
+let myCreatedDate = new Date("02-10-2024")
 
-// //  let myCreatedDate = new Date(2023,11, 23)
-//  let myCreatedDate = new Date("02-10-2024")
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDates.toDateString());
 
-// //  console.log(myCreatedDate.toDateString());
-
-//   let myTimeStanp = Date.now()
-// //  console.log(myTimeStanp);//time ke milisecand a niye quize app ba ke age uplod korcge dekhte help kore
+let myTimeStanp = Date.now()
+// console.log(myTimeStanp);//time ke milisecand a niye quize app ba ke age uplod korcge dekhte help kore
 //   // console.log(myCreatedDate.getTime());
 // // console.log(Math.floor(Date.now()/1000)); //value flotting point a ale take single unit a nite 
 
@@ -28,6 +29,15 @@ console.log(myDate.toDateString());
 //     weekday: "long",
     
 // })
+
+let myDateontimestamp = Date.now();
+console.log(myDateontimestamp);
+
+let myDateis = new Date("02-01-2024")
+console.log(myDateis.toDateString());
+
+
+
 
 
 
