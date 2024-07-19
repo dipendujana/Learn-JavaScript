1a8e0269f5157eef959f863d7e20f7bbfc3c9ca9
dipendujana <120547835+dipendujana@users.noreply.github.com>
2024-07-19 14:57:22 +0000

    all arryas

--- 02_basics/02_arryas.js
+++ 02_basics/02_arryas.js
@@ -1,7 +1,7 @@
 const marvel_heros = ["thor", "Ironman", "spiderman"]
 const dc_heros = ["superman", "flash", "batman"]
 
-marvel_heros.push(dc_heros)
+// marvel_heros.push(dc_heros)
 
 //  console.log(marvel_heros);
 //  console.log(marvel_heros[3][1]);
@@ -14,8 +14,13 @@ const all_new_heros = [...marvel_heros, ...dc_heros]  // by the help of sperad o
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
-const real_another_array = another_array.flat(Infinity) //" flat " ar shaje one gulo arrya ke ek songe kora jay 
-console.log(real_another_array);
+const real_another_array = another_array.flat(Infinity) //" flat " ar shaje onek gulo arrya ke ek songe kora jay 
+// console.log(real_another_array);
+//   [
+//     1, 2, 3, 4, 5,
+//     6, 7, 6, 7, 4,
+//     5
+//   ]
 
 // console.log(Array.isArray("Hitesh"))
 // console.log(Array.from("Hitesh"))
@@ -25,5 +30,6 @@ let score1 = 100
 let score2 = 200
 let score3 = 300
 
-// console.log(Array.of(score1, score2, score3));
+console.log(Array.of(score1, score2, score3));
+// console.log(Array.from(score1, score2, score3));
 
