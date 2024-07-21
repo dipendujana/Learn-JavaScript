4738c215f8d9e3ecfd3abc95f61d0e9efe4a8883
dipendujana <120547835+dipendujana@users.noreply.github.com>
2024-07-21 11:15:04 +0000

    arrya pat 2

--- 02_basics/02_arryas.js
+++ 02_basics/02_arryas.js
@@ -1,10 +1,10 @@
 const marvel_heros = ["thor", "Ironman", "spiderman"]
 const dc_heros = ["superman", "flash", "batman"]
 
-// marvel_heros.push(dc_heros)
+marvel_heros.push(dc_heros)
 
-//  console.log(marvel_heros);
-//  console.log(marvel_heros[3][1]);
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
 
 const allHeros = marvel_heros.concat(dc_heros)
 // console.log(allHeros);
@@ -22,14 +22,14 @@ const real_another_array = another_array.flat(Infinity) //" flat " ar shaje onek
 //     5
 //   ]
 
-// console.log(Array.isArray("Hitesh"))
-// console.log(Array.from("Hitesh"))
-// console.log(Array.from({name: "hitesh"})) // interesting
+console.log(Array.isArray("Hitesh"))
+console.log(Array.from("Hitesh"))
+console.log(Array.from({name: "hitesh"})) // interesting
 
 let score1 = 100
 let score2 = 200
 let score3 = 300
 
 console.log(Array.of(score1, score2, score3));
-// console.log(Array.from(score1, score2, score3));
+
 
