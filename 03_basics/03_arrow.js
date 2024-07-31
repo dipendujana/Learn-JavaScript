4f861f6c8ee164fa2a6c5a373c54948e4cffcfc9
dipendujana <120547835+dipendujana@users.noreply.github.com>
2024-07-31 04:59:35 +0000

    arrow

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -4,13 +4,14 @@ const user = {
 
     welcomeMessage: function() {
         console.log(`${this.username} , welcome to website`);
-        console.log(this);
+        // console.log(this);
     }
 
 }
 // user.welcomeMessage()
-// user.username = "Sam"
+user.username = "Sam"
 // user.welcomeMessage()
+
 
 //console.log(this);
 
@@ -26,25 +27,25 @@ const user = {
 // }
 
 
-const chai = () => {
-    let username = "dipendu"         
-    console.log(this);
-}
-// 
+// const chai = () => {
+//     let username = "dipendu"         
+//     console.log(this);
+// }
+// chai()
 
 // const addTwo = (num1 , num2) => {
 //     return num1 + num2              // Basics Arrow function //explisite return
 // }
 
-//const addTwo = (num1 , num2) =>  num1 + num2       // this is the sentex of impleset return
+// const addTwo = (num1 , num2) =>  num1 + num2       // this is the sentex of impleset return
 
-// const addTwo = (num1 , num2) =>  (num1 + num2)
-// console.log(addTwo(3 ,5));
+const addTwo = (num1 , num2) =>  (num1 + num2)
+console.log(addTwo(3 ,5));
 
 // *note { } carlibases thakle return keyword dite hoy { return num1 + num2 }
 // *note ( ) carlibases thakle return keyword dite hobe ni  (num1 + num2)
 
-const addTwo = (num1 , num2) => ( {username: "dipensu"})
-console.log(addTwo(3 ,5));
+// const addTwo = (num1 , num2) => ( {username: "dipensu"})
+// console.log(addTwo(3 ,5));
 
 // const myArrat = [2,3,4,5]
